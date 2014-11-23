2c538dd6285c7ab24f0e5fac51ed5c11500232be
lukaswhite <hello@lukaswhite.com>
2014-11-23 11:07:45 +0000

    Fixed issue with crop() and gravity

--- easyimage.js
+++ easyimage.js
@@ -1,6 +1,7 @@
 var Q = require('q');
-var exec = require('child_process').execFile;
-var child, args;
+var exec = require('child_process').exec;
+var child;
+var imcmd;
 
 var error_messages = {
 	'path': 'Missing image paths.\nMake sure both source and destination files are specified.',
@@ -14,20 +15,21 @@ function info(file) {
 
 	var deferred = Q.defer();
 
-	//file = quoted_name(file);
+	file = quoted_name(file);
 	// %z = depth, %m = type, %w = width, %h = height, %b = filesize in byte, %f = filename, %x = density
-	args = ['-format', '%m %z %w %h %b %x %f', file];
+	imcmd = 'identify -format "%m %z %w %h %b %x %f" ' + file;
 
-	child = exec('identify', args, function(err, stdout, stderr) {
+	child = exec(imcmd, function(err, stdout, stderr) {
 		var info = {};
-
 		//Basic error handling
-		if (stdout) {
+		if (stderr.match(/^identify:/)) {
+			deferred.reject(new Error(error_messages['unsupported']));
+		} else {
+
 			var temp = stdout.replace('PixelsPerInch', '').replace('PixelsPerCentimeter', '').split(' ');
-			
 			//Basic error handling:
 			if (temp.length < 7) {
-				deferred.reject(new Error(stderr || error_messages['unsupported']));
+				deferred.reject(new Error(error_messages['unsupported']));
 			} else {
 
 				info.type    = temp[0];
@@ -38,14 +40,8 @@ function info(file) {
 				info.density = parseFloat(temp[5]);
 				info.name    = temp.slice(6).join(' ').replace(/(\r\n|\n|\r)/gm, '').trim();
 
-				if (stderr) {
-					info.warnings = stderr.split('\n');
-				}
-
 				deferred.resolve(info);
 			}
-		} else {
-			deferred.reject(new Error(stderr || error_messages['unsupported']));
 		}
 	});
 
@@ -74,13 +70,11 @@ exports.convert = function(options) {
 
 		if (options.src === undefined || options.dst === undefined) return deferred.reject(error_messages['path']);
 
-		// options.src = quoted_name(options.src);
-		// options.dst = quoted_name(options.dst);
-		if (options.quality === undefined) args = [options.src, options.dst];
-		else args = [options.src, '-quality', options.quality, options.dst];
-
-		child = exec('convert', args, function(err, stdout, stderr) {
-
+		options.src = quoted_name(options.src);
+		options.dst = quoted_name(options.dst);
+		if (options.quality === undefined) imcmd = 'convert ' + options.src + ' ' + options.dst;
+		else imcmd = 'convert ' + options.src + ' -quality ' + options.quality + ' ' + options.dst;
+		child = exec(imcmd, function(err, stdout, stderr) {
 			if (err) deferred.reject(err);
 			else deferred.resolve(info(options.dst));
 		});
@@ -101,12 +95,11 @@ exports.resize = function(options) {
 		if (options.width === undefined) return deferred.reject(error_messages['dim']);
 
 		options.height = options.height || options.width;
-		// options.src = quoted_name(options.src);
-		// options.dst = quoted_name(options.dst);
-		if (options.quality === undefined) args = [options.src, '-resize', options.width + 'x' + options.height, options.dst];
-		else args = [options.src, '-resize', options.width + 'x' + options.height, '-quality', options.quality, options.dst];
-
-		child = exec('convert', args, function(err, stdout, stderr) {
+		options.src = quoted_name(options.src);
+		options.dst = quoted_name(options.dst);
+		if (options.quality === undefined) imcmd = 'convert ' + options.src + ' -resize '+options.width + 'x' + options.height + ' ' + options.dst;
+		else imcmd = 'convert ' + options.src + ' -resize '+options.width + 'x' + options.height + ' -quality ' + options.quality + ' ' + options.dst;
+		child = exec(imcmd, function(err, stdout, stderr) {
 			if (err) deferred.reject(err);
 			deferred.resolve(info(options.dst));
 		});		
@@ -128,13 +121,13 @@ exports.crop = function(options) {
 	options.gravity = options.gravity || 'Center';
 	options.x = options.x || 0;
 	options.y = options.y || 0;
-	// options.src = quoted_name(options.src);
-	// options.dst = quoted_name(options.dst);
+	options.src = quoted_name(options.src);
+	options.dst = quoted_name(options.dst);
 
-	if (options.quality === undefined) args = [options.src, '-gravity', options.gravity, '-crop', options.cropwidth + 'x'+ options.cropheight + '+' + options.x + '+' + options.y, options.dst];
-	else args = [options.src, '-gravity', options.gravity, '-crop', options.cropwidth + 'x'+ options.cropheight + '+' + options.x + '+' + options.y, '-quality', options.quality, options.dst];
-
-	child = exec('convert', args, function(err, stdout, stderr) {
+	//if (options.quality === undefined) imcmd = 'convert ' + options.src + ' -gravity ' + options.gravity + ' -crop '+ options.cropwidth + 'x'+ options.cropheight + '+' + options.x + '+' + options.y + ' ' + options.dst;
+	if (options.quality === undefined) imcmd = 'convert ' + options.src + ' -crop '+ options.cropwidth + 'x'+ options.cropheight + '+' + options.x + '+' + options.y + ' ' + options.dst;
+	else  imcmd = 'convert ' + options.src + ' -gravity ' + options.gravity + ' -crop '+ options.cropwidth + 'x'+ options.cropheight + '+' + options.x + '+' + options.y + ' -quality ' + options.quality + ' ' + options.dst;
+	child = exec(imcmd, function(err, stdout, stderr) {
 		if (err) deferred.reject(err);
 		deferred.resolve(info(options.dst));
 	});
@@ -160,13 +153,12 @@ exports.rescrop = function(options) {
 		options.gravity = options.gravity || 'Center';
 		options.x = options.x || 0;
 		options.y = options.y || 0;
-		// options.src = quoted_name(options.src);
-		// options.dst = quoted_name(options.dst);
+		options.src = quoted_name(options.src);
+		options.dst = quoted_name(options.dst);
 		options.fill = options.fill ? '^' : '';
-		if (options.quality === undefined) args = [options.src, '-resize', options.width + 'x' + options.height + options.fill, '-gravity', options.gravity, '-crop', options.cropwidth + 'x'+ options.cropheight + '+' + options.x + '+' + options.y, options.dst];
-		else args = [options.src, '-resize', options.width + 'x' + options.height, options.fill, '-gravity' + options.gravity, '-crop', options.cropwidth + 'x'+ options.cropheight + '+' + options.x + '+' + options.y, '-quality', options.quality, options.dst];
-
-		child = exec('convert', args, function(err, stdout, stderr) {
+		if (options.quality === undefined) imcmd = 'convert ' + options.src + ' -resize ' + options.width + 'x' + options.height + options.fill + ' -gravity ' + options.gravity + ' -crop '+ options.cropwidth + 'x'+ options.cropheight + '+' + options.x + '+' + options.y + ' ' + options.dst;
+		else imcmd = 'convert ' + options.src + ' -resize ' + options.width + 'x' + options.height + options.fill + ' -gravity ' + options.gravity + ' -crop '+ options.cropwidth + 'x'+ options.cropheight + '+' + options.x + '+' + options.y + ' -quality ' + options.quality + ' ' + options.dst;
+		child = exec(imcmd, function(err, stdout, stderr) {
 			if (err) deferred.reject(err);
 			deferred.resolve(info(options.dst));
 		});		
@@ -190,8 +182,8 @@ exports.thumbnail = function(options) {
 		options.gravity = options.gravity || 'Center';
 		options.x = options.x || 0;
 		options.y = options.y || 0;
-		// options.src = quoted_name(options.src);
-		// options.dst = quoted_name(options.dst);
+		options.src = quoted_name(options.src);
+		options.dst = quoted_name(options.dst);
 
 		info(options.src).then(function(original) {
 
@@ -206,10 +198,10 @@ exports.thumbnail = function(options) {
 			else if (original.height > original.width) { resizeheight = ''; }
 
 			// resize and crop
-			if (options.quality === undefined) args = [options.src, '-interpolate', 'bicubic', '-strip', '-thumbnail',  resizewidth + 'x' + resizeheight, '-gravity', options.gravity, '-crop', options.width + 'x'+ options.height + '+' + options.x + '+' + options.y, options.dst];
-			else args = [options.src, '-interpolate', 'bicubic', '-strip', '-thumbnail', resizewidth + 'x' + resizeheight, '-quality', options.quality, '-gravity', options.gravity, '-crop', options.width + 'x'+ options.height + '+' + options.x + '+' + options.y, options.dst];
+			if (options.quality === undefined) imcmd = 'convert ' + options.src + ' -interpolate bicubic -strip -thumbnail ' + resizewidth + 'x' + resizeheight + ' -gravity ' + options.gravity + ' -crop '+ options.width + 'x'+ options.height + '+' + options.x + '+' + options.y + ' ' + options.dst;
+			else imcmd = 'convert ' + options.src + ' -interpolate bicubic -strip -thumbnail '+ resizewidth + 'x' + resizeheight + ' -quality ' + options.quality + ' -gravity ' + options.gravity + ' -crop '+ options.width + 'x'+ options.height + '+' + options.x + '+' + options.y + ' ' + options.dst;
 
-			child = exec('convert', args, function(err, stdout, stderr) {
+			child = exec(imcmd, function(err, stdout, stderr) {
 				if (err) return deferred.reject(err);
 				deferred.resolve(info(options.dst));
 			});
@@ -221,16 +213,18 @@ exports.thumbnail = function(options) {
 	return deferred.promise;
 };
 
-// issue your own ImageMagick command
+// for the hardcore types - issue your own ImageMagick command
 exports.exec = function(command) {
 
 	var deferred = Q.defer();
 
 	process.nextTick(function () {
 
-		args = command.split(' ').slice(1);
+		var _command = command.split(' ')[0];
+		// as a security measure, we will allow only 'convert' commands
+		if (_command != 'convert') return deferred.reject(error_messages['restricted']);
 
-		child = exec('convert', args, function(err, stdout, stderr) {
+		child = exec(command, function(err, stdout, stderr) {
 			if (err) return deferred.reject(err);
 			deferred.resolve(stdout);
 		});		
